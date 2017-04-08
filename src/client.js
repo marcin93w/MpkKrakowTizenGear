bbd5ee95214a8297d6d03cfd278fb88c6e9b13ee
marcin93w <marcin93w@gmail.com>
2017-04-08 22:23:50 +0200

    Added polyfills to work on tizen browser

--- src/client.js
+++ src/client.js
@@ -1,3 +1,4 @@
+import "babel-polyfill";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
@@ -5,28 +6,32 @@ import { Provider } from 'react-redux';
 import App from './containers/App';
 import configureStore from './stores';
 
-const store = configureStore();
+try{
+  const store = configureStore();
 
-ReactDOM.render(
-  <AppContainer>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </AppContainer>,
-  document.getElementById('app')
-);
+  ReactDOM.render(
+    <AppContainer>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </AppContainer>,
+    document.getElementById('app')
+  );
 
-if (module.hot) {
-  module.hot.accept('./containers/App', () => {
-    const NextApp = require('./containers/App').default; // eslint-disable-line global-require
+  if (module.hot) {
+    module.hot.accept('./containers/App', () => {
+      const NextApp = require('./containers/App').default; // eslint-disable-line global-require
 
-    ReactDOM.render(
-      <AppContainer>
-        <Provider store={store}>
-          <NextApp />
-        </Provider>
-      </AppContainer>,
-      document.getElementById('app')
-    );
-  });
+      ReactDOM.render(
+        <AppContainer>
+          <Provider store={store}>
+            <NextApp />
+          </Provider>
+        </AppContainer>,
+        document.getElementById('app')
+      );
+    });
+  }
+} catch(error) {
+  document.getElementById('app').innerHTML = '<br /><br /><br /><br /><br />' + error;
 }
